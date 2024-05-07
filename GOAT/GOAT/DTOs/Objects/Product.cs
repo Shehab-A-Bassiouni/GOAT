e0cf77b375247df0eb1ad2252b612fea2989b04b
Shehab-A-Bassiouni <swe.shehab.a.bassiouni@gmail.com>
2024-05-07 05:11:33 +0300

    push khaled files

---
+++ GOAT/GOAT/DTOs/Objects/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.Objects
+{
+    public class Product : BaseEntity
+    {
+        public required string Serial { get; set; }  //
+        public required string Name { get; set; } //
+        public string? Description { get; set; } //
+        [ForeignKey("Category")]
+        public required int CategoryID { get; set; }  //
+        public required Category Category { get; set; }  //
+        public required string ImagePath { get; set; }  //
+        public required int Stock { get; set; }  //
+        public required int Price { get; set; }  //
+        public required string Size { get; set; }  //
+        public string? Colors { get; set; }  //
+        [ForeignKey("RelatedProducts")]
+        public  int? RelatedProductsID { get; set; }
+        public  RelatedProducts? RelatedProducts { get; set; }
+
+    }
+}
---
+++ GOAT/GOAT/DTOs/Objects/RelatedProducts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.Objects
+{
+    public class RelatedProducts : BaseEntity
+    {
+        public required string Name { get; set; }
+        public required List<Product> Products { get; set; }
+    }
+}
