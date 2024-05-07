3ffc809ff8e5947dcd5e40485b28a1b41efba11d
Shehab-A-Bassiouni <swe.shehab.a.bassiouni@gmail.com>
2024-05-07 05:10:33 +0300

    del

--- GOAT/GOAT/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GOAT.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
